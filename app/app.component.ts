7c02ba774db2dadba578a58ce3a8172b8506bffc
damianwebelevate <damian.orourke@webelevate.ie>
2016-09-09 15:09:33 +0100

    see if im in my branch

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: `<h1>Beginning.....</h1>
+  template: `<h1>Branch???</h1>
   <p>multi-line declaration template</p>
   `
 })
