54a2fb2f78a4e8aa334bcdb69cd6d6dea860d89d
damianwebelevate <damian.orourke@webelevate.ie>
2016-09-09 13:25:01 +0100

    basic spp structure

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: `<h1>Beginning.....</h1>
+  <p>multi-line declaration template</p>
+  `
+})
+export class AppComponent { }
