60fe17168128e7eacfdc5cde03476dcdc63ada8c
damianwebelevate <damian.orourke@webelevate.ie>
2016-09-19 12:46:53 +0100

    tutorial 5 up to date

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,29 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { NgModule }       		from '@angular/core';
+import { routing } 				from './app.routing';
+import { BrowserModule }  		from '@angular/platform-browser';
+import { FormsModule }    		from '@angular/forms';
+import { AppComponent }        	from './app.component';
+import { HeroDetailComponent } 	from './hero-detail.component';
+import { HeroesComponent }     	from './heroes.component';
+import { HeroService }         	from './hero.service';
+import { DashboardComponent } 	from './dashboard.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
