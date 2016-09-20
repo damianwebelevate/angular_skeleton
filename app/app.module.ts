57ef320aaa8d69775b7c34270d2923bfcc32e33f
damianwebelevate <damian.orourke@webelevate.ie>
2016-09-20 09:55:26 +0100

    converted the mock heroes file to a simulated in-memory db stroke web api

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,16 +2,25 @@ import { NgModule }             from '@angular/core';
 import { routing }              from './app.routing';
 import { BrowserModule }        from '@angular/platform-browser';
 import { FormsModule }          from '@angular/forms';
+import { HttpModule }           from '@angular/http';
+
+// imports for loading & config the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent }        	from './app.component';
 import { HeroDetailComponent } 	from './hero-detail.component';
 import { HeroesComponent }     	from './heroes.component';
 import { HeroService }         	from './hero.service';
 import { DashboardComponent } 	from './dashboard.component';
 
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     routing
   ],
   declarations: [
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,17 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable }     from '@angular/core';
+import { Headers, Http }  from '@angular/http';
+
+// importing the toPromise method
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+
 
 @Injectable()
 export class HeroService {
+
+  private heroesUrl = 'app/heroes'; //URL to web api
+
+  constructor(private http: Http){ }
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then(response => response.json().data as Hero[])
+               .catch(this.handleError);
   }
   getHero(id: number): Promise<Hero> {
   	return this.getHeroes()
   			   .then(heroes => heroes.find(hero => hero.id === id));
   }
+
   // See the "Take it slow" appendix
   getHeroesSlowly(): Promise<Hero[]> {
     return new Promise<Hero[]>(resolve =>
